cee8a50384feab19a61d06e4f2300d34cb337f86
lisa-wolfgang <lisawolfgang.onscratch@gmail.com>
2021-05-11 20:22:55 -0500

    Incomplete

---
+++ addons/block-cherry-picking/userscript.js
@@ -0,0 +1,41 @@
+import UndoGroup from "/addons/editor-devtools/blockly/UndoGroup.js";
+
+export default async function ({ addon, global, console }) {
+  
+  const BlocklyInstance = await addon.tab.traps.getBlockly();
+  const originalDraggingObject = BlocklyInstance.BlockSvg.prototype.setDragging;
+  var ctrlMode = addon.settings.get("ctrlMode")
+  console.log('Cherry-picking addon is enabled')
+
+  BlocklyInstance.BlockSvg.prototype.setDragging = function(adding) {
+    console.log(this)
+    if (ctrlMode === "fullEnable") {
+      // Mostly taken from editor-devtools
+      let workspace = BlocklyInstance.getMainWorkspace()
+      let dataId = this && this.id;
+      let block = workspace.getBlockById(dataId);
+      if (block) {
+        block.select()
+        let next = block.getNextBlock();
+        if (next) {
+          next.unplug(false);
+        }
+
+        if (next) {
+          workspace.undo();
+        }
+        let block = wksp.getBlockById(dataId);
+        UndoGroup.startUndoGroup(wksp);
+        block.dispose(true);
+        UndoGroup.endUndoGroup(wksp);
+      }
+
+    }
+    return originalDraggingObject.call(this, adding);
+  };
+
+  addon.settings.addEventListener("change", function () {
+    ctrlMode = addon.settings.get("ctrlMode")
+  });
+
+}
