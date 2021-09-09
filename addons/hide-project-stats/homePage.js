49d29380ddfa6fe399cbc50d04f476d585d1cd3e
lisa-wolfgang <lisa-wolfgang@users.noreply.github.com>
2021-09-09 03:39:21 +0000

    Format code

--- addons/hide-project-stats/homePage.js
+++ addons/hide-project-stats/homePage.js
@@ -5,8 +5,8 @@ export default async function ({ addon, global, console, msg, safeMsg: m }) {
   // show the remix and love counts if the user owns the projects.
   // Figure out who each project's creator is and show the stats if that project creator is the user.
   function refreshLabels() {
-    document.querySelectorAll(".thumbnail").forEach(element => {
-      console.log(element)
+    document.querySelectorAll(".thumbnail").forEach((element) => {
+      console.log(element);
       if (!addon.self.disabled && addon.settings.get("showOwnStats")) {
         let creator = element.getElementsByClassName("thumbnail-creator")[0].querySelector("a").innerText;
         if (creator == addon.auth.fetchUsername()) {
@@ -20,7 +20,13 @@ export default async function ({ addon, global, console, msg, safeMsg: m }) {
 
   refreshLabels();
 
-  addon.settings.addEventListener("change", () => { refreshLabels(); });
-  addon.self.addEventListener("disabled", () => { refreshLabels(); });
-  addon.self.addEventListener("reenabled", () => { refreshLabels(); });
+  addon.settings.addEventListener("change", () => {
+    refreshLabels();
+  });
+  addon.self.addEventListener("disabled", () => {
+    refreshLabels();
+  });
+  addon.self.addEventListener("reenabled", () => {
+    refreshLabels();
+  });
 }
