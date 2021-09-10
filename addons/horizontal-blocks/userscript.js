10cca75235eff976610af4f593f03d63ecfd601e
lisa-wolfgang <lisawolfgang.onscratch@gmail.com>
2021-09-09 21:53:10 -0500

    Initial commit (unfinished)

---
+++ addons/horizontal-blocks/userscript.js
@@ -0,0 +1,8 @@
+export default async function ({ addon, global, console }) {
+  let BlocklyInstance = await addon.tab.traps.getBlockly();
+  let originalObject = BlocklyInstance.Options;
+  BlocklyInstance.Options = function (options) {
+    this.horizontalLayout = true;
+    return originalObject.call(this, options);
+  }
+}
