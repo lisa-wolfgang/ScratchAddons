5950ce908e01a477c7549f6bdd8331ae7db2cd0f
lisa-wolfgang <lisawolfgang.onscratch@gmail.com>
2021-09-11 09:45:18 -0500

    Initial commit

---
+++ addons/insert-inputs/userscript.js
@@ -0,0 +1,11 @@
+export default async function ({ addon, global, console }) {
+  const BlocklyInstance = await addon.tab.traps.getBlockly();
+  const originalAppendInput = BlocklyInstance.Block.prototype.appendInput_;
+
+  // `opt_healStack` is a built-in option in scratch-blocks that enables cherry-picking behavior.
+  // All this function does is enable that built-in option for every block.
+  BlocklyInstance.BlockSvg.prototype.unplug = function (opt_healStack) {
+    
+    return originalObject.call(this, opt_healStack);
+  };
+}
