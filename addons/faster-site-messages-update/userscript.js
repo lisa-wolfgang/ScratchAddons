a5eb8bfec19e696bf8e55bb1d3fbb8b26ae87fb6
lisa-wolfgang <lisawolfgang.onscratch@gmail.com>
2022-01-31 16:27:48 -0600

    Squashed commit of the following:
    commit 0441650ee8c4270bb2c8087d21a8c4a06a4ef10f
    Author: hello-smile6 <73048226+hello-smile6@users.noreply.github.com>
    Date:   Mon Jan 3 17:41:01 2022 -0800
    
        Fix userscript.js
    
        Shouldn't even keep working on this crap. But I will. Resolves https://github.com/ScratchAddons/ScratchAddons/pull/4006#issuecomment-1003774158
    
    commit f6cc1ef76ebff074627941ce13c7e28689393d85
    Merge: 9aec30b4 6d398661
    Author: hello-smile6 <73048226+hello-smile6@users.noreply.github.com>
    Date:   Mon Jan 3 17:38:36 2022 -0800
    
        Merge branch 'ScratchAddons:master' into add-faster-site-messages-update
    
    commit 9aec30b4005c09b31d9ef8a1a8e57e198aeedfc0
    Author: hello-smile6 <73048226+hello-smile6@users.noreply.github.com>
    Date:   Thu Dec 30 15:54:03 2021 -0800
    
        Fix error, run prettier
    
    commit 50d5aca7fe9d38bfc3300a9883cbab39e7f8bc3f
    Author: hello-smile6 <73048226+hello-smile6@users.noreply.github.com>
    Date:   Thu Dec 30 14:24:24 2021 -0800
    
        Run prettier
    
    commit 0a78bcf866df1f7cea185960e1ec479c789620ff
    Author: hello-smile6 <73048226+hello-smile6@users.noreply.github.com>
    Date:   Thu Dec 30 14:24:09 2021 -0800
    
        Fix undefined message count
    
    commit 787a847fc1ee50a207fc030ec3eb6f8477cb0dcc
    Author: hello-smile6 <73048226+hello-smile6@users.noreply.github.com>
    Date:   Thu Dec 30 14:09:42 2021 -0800
    
        Run prettier again
    
    commit dd485381ddd71f55d20cdb01627c2c1b0d2ad571
    Author: hello-smile6 <73048226+hello-smile6@users.noreply.github.com>
    Date:   Thu Dec 30 14:09:29 2021 -0800
    
        Improve garbage collection
    
        References are destroyed as soon as possible now.
    
    commit fbf25004facaa105ddd4453da23111f1623d21d7
    Author: hello-smile6 <73048226+hello-smile6@users.noreply.github.com>
    Date:   Thu Dec 30 14:03:58 2021 -0800
    
        Run prettier
    
        I noticed that most people don't actually use it, but I decided to use it, since there's a config file already.
    
    commit 665284973a774234a9afb119210bd8bd14cd79e7
    Author: hello-smile6 <73048226+hello-smile6@users.noreply.github.com>
    Date:   Thu Dec 30 13:53:33 2021 -0800
    
        Don't run on editor pages
    
        Not exactly the best for accuracy, but I don't know how to do better. Maybe @SheepTester could help.
    
    commit 37951f34ac88ea4468f5b9d33902e3a1aee2ffa8
    Author: hello-smile6 <73048226+hello-smile6@users.noreply.github.com>
    Date:   Thu Dec 30 13:49:36 2021 -0800
    
        Consolidate to 1 script
    
    commit 41875d98d0b6dff94927da79829ff0ea2ae4fbcc
    Author: hello-smile6 <73048226+hello-smile6@users.noreply.github.com>
    Date:   Thu Dec 30 13:48:47 2021 -0800
    
        Remove debug logging
    
    commit 067ebf5e69c85b724961e4daadd5893a461ebb0e
    Author: hello-smile6 <73048226+hello-smile6@users.noreply.github.com>
    Date:   Thu Dec 30 13:43:38 2021 -0800
    
        Dynamic element reference
    
    commit ef8c2afd4a174fe792b4630ef379d42d51237c47
    Author: hello-smile6 <73048226+hello-smile6@users.noreply.github.com>
    Date:   Thu Dec 30 13:39:20 2021 -0800
    
        Get www to work
    
    commit a5eea60145a94cd196d2ffab9f65fba7ce917010
    Author: hello-smile6 <73048226+hello-smile6@users.noreply.github.com>
    Date:   Thu Dec 30 13:15:31 2021 -0800
    
        Add addon
    
        Let's get it working, now!

---
+++ addons/faster-site-messages-update/userscript.js
@@ -0,0 +1,38 @@
+export default async function ({ addon, console, setTimeout, setInterval, clearTimeout, clearInterval }) {
+  if (addon.tab.clientVersion === null) return;
+  if (window.location.href.includes("/projects") && window.location.href.includes("/editor")) return;
+  let msgCount;
+  // console.log("Hi from the addon!")
+  const setBadge = () => {
+    if (msgCount === null) {
+      // Do nothing
+    } else {
+      var targetElement;
+      if (addon.tab.clientVersion === "scratch-www") {
+        targetElement = document.querySelector(
+          "#navigation > div > ul > li.link.right.messages > a > span.message-count.show"
+        );
+      } else {
+        targetElement = document.querySelector(
+          "#topnav > div > div > ul.account-nav.logged-in > li.messages > a > span"
+        );
+      }
+      // console.log(document)
+      if (typeof msgCount == "number") {
+        targetElement.innerText = msgCount;
+      } else {
+        // Assume that message count check failed
+        console.error("Message count check failed. Result was invalid.\n\nResult was:\n".concat(msgCount));
+      }
+      targetElement = null; // Garbage collection
+    }
+  };
+  const getMsgCountAndSetBadge = async () => {
+    msgCount = await addon.account.getMsgCount();
+    setBadge();
+    msgCount = null; // Garbage collection
+  };
+
+  getMsgCountAndSetBadge();
+  window.setInterval(getMsgCountAndSetBadge, 30000);
+}
