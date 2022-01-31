4c1436f5c767324af7d2a222a39b61ff815ad317
lisa-wolfgang <lisawolfgang.onscratch@gmail.com>
2022-01-31 16:52:08 -0600

    Squashed commit of the following:
    commit 35fef5e5fff4da33403aa84e6732388bd0c28378
    Author: Samq64 <81489795+Samq64@users.noreply.github.com>
    Date:   Sat Jan 22 17:36:34 2022 -0500
    
        Update addon.json
    
    commit dc4937a10e77e55b3fe00a231b566f0f35de6786
    Merge: 522cf283 66014bf4
    Author: Samq64 <81489795+Samq64@users.noreply.github.com>
    Date:   Sat Jan 22 17:19:05 2022 -0500
    
        Merge branch 'ScratchAddons:master' into scroll-comments
    
    commit 522cf283338d209dfe8881125d13f3d8e7a12dd0
    Author: Samq64 <Samq64@users.noreply.github.com>
    Date:   Sat Dec 11 23:51:11 2021 +0000
    
        Format code
    
    commit aa483c6901b1eff8394963b0fae5a89f9ca91e13
    Author: Samq64 <81489795+Samq64@users.noreply.github.com>
    Date:   Sat Dec 11 18:50:38 2021 -0500
    
        Add myself to the add-on credits
    
    commit 0417b897362bc30c13b373f99f372e9a65ad05f9
    Author: Samq64 <81489795+Samq64@users.noreply.github.com>
    Date:   Sat Dec 11 09:13:46 2021 -0500
    
        add height to latestUpdate
    
    commit fd28f1dd8a7a792fa9a1c3fccfc773eb5e0d3d6d
    Author: Samq64 <81489795+Samq64@users.noreply.github.com>
    Date:   Sat Dec 11 09:12:07 2021 -0500
    
        fix errors
    
    commit 4f3fe9a1a6cd630be7773c7021ce4a5cb6e674f5
    Author: Samq64 <Samq64@users.noreply.github.com>
    Date:   Sat Dec 11 14:06:48 2021 +0000
    
        Format code
    
    commit c8c8ecae9206278145aaa400cbf797140d01f3a1
    Author: Samq64 <81489795+Samq64@users.noreply.github.com>
    Date:   Sat Dec 11 09:06:13 2021 -0500
    
        make max profile comment height configurable
    
    commit dd71524dd513c4d4f980bdba9eeba56d8790c5b2
    Author: Samq64 <Samq64@users.noreply.github.com>
    Date:   Sat Dec 11 14:05:38 2021 +0000
    
        Format code
    
    commit 00c78d56e452d013ebc5226c1fca614d0b8f15e2
    Author: Samq64 <81489795+Samq64@users.noreply.github.com>
    Date:   Sat Dec 11 09:05:00 2021 -0500
    
        make www max comment height configurable
    
    commit 7c88bb7512f86026cb2a1218bd040b6a3cf9e4e9
    Author: Samq64 <81489795+Samq64@users.noreply.github.com>
    Date:   Sat Dec 11 09:03:45 2021 -0500
    
        Update addon.json
    
    commit f44280ee3dcae302f77ec291b6ba1871dff0341e
    Author: Samq64 <Samq64@users.noreply.github.com>
    Date:   Fri Dec 10 02:07:47 2021 +0000
    
        Format code
    
    commit 1c82ae11f66e138481ef7f54a6952e09c096f057
    Author: Samq64 <81489795+Samq64@users.noreply.github.com>
    Date:   Thu Dec 9 21:07:07 2021 -0500
    
        Increase scratch-www max comment height to 120
    
        This makes the number of visible comment line consistent between all pages.
    
    commit c4e3911257d960cdb7be5faa1baf2e14f64b4940
    Author: Samq64 <81489795+Samq64@users.noreply.github.com>
    Date:   Thu Dec 9 17:59:11 2021 -0500
    
        Add latestUpdate
    
    commit e246988d60245b5b5a78e0083c85d5762666f7bb
    Author: Samq64 <81489795+Samq64@users.noreply.github.com>
    Date:   Thu Dec 9 17:24:05 2021 -0500
    
        Oops!
    
    commit b8f65b1901f3cc2ab42968f9af38454bf1dbd7a0
    Author: Samq64 <81489795+Samq64@users.noreply.github.com>
    Date:   Thu Dec 9 17:06:12 2021 -0500
    
        Fix scrollbars setting check and format code
    
    commit 8b60c06da741d73bc0b8ca24ffe87db5f205e2bb
    Author: Samq64 <81489795+Samq64@users.noreply.github.com>
    Date:   Thu Dec 9 16:55:53 2021 -0500
    
        scrollable profile comments
    
    commit f62bf391b3f62c839972f0a19f67dcd2cf4237ca
    Author: Samq64 <81489795+Samq64@users.noreply.github.com>
    Date:   Thu Dec 9 16:55:04 2021 -0500
    
        scrollable scratch-www comments
    
    commit 2c9ae519d8f2b6b16f172e0f4f68f6ccf00ce922
    Author: Samq64 <81489795+Samq64@users.noreply.github.com>
    Date:   Thu Dec 9 16:53:44 2021 -0500
    
        add scroll bar toggle and scrollbar user style

--- addons/comments-linebreaks/userscript.js
+++ addons/comments-linebreaks/userscript.js
@@ -4,6 +4,10 @@ export default async function ({ addon, global, console }) {
   while (true) {
     const comment = await addon.tab.waitForElement(".comment .content", { markAsSeen: true });
     comment.style.whiteSpace = "break-spaces";
+    if (addon.settings.get("scrollbars")) {
+      comment.style.maxHeight = addon.settings.get("height") * 20 + "px";
+      comment.style.overflow = "auto";
+    }
     formatProfileComments(comment);
   }
 }
