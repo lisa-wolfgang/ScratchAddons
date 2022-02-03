b36129f9215c3174ba5300af5a6927fecbe645a1
lisa-wolfgang <lisawolfgang.onscratch@gmail.com>
2022-02-03 16:53:59 -0600

    New addon: `colored-follower-projects`
    commit 98080cf27f499fc170b3ee8efdfdac2a9127ca32
    Author: A person somewhere that does something for the reason that he likes to do that thing. I really don't know what I'm saying, but I do know I can make this name 255 characters, so I'm just going to put a bunch of "a"s until I get to 255 aaaaaaaaaaaaaaaaaaaaa <55938439+codingMASTER398@users.noreply.github.com>
    Date:   Sun Jul 25 16:06:39 2021 +1000
    
        Update userscript.js
    
    commit 307950deb2533716a4992812e89c12345c3a4e2c
    Author: A person somewhere that does something for the reason that he likes to do that thing. I really don't know what I'm saying, but I do know I can make this name 255 characters, so I'm just going to put a bunch of "a"s until I get to 255 aaaaaaaaaaaaaaaaaaaaa <55938439+codingMASTER398@users.noreply.github.com>
    Date:   Sun Jul 25 12:04:14 2021 +1000
    
        Update addon.json
    
    commit a3c48b11c7adc6245aa2146f25bd3398478feff0
    Author: A person somewhere that does something for the reason that he likes to do that thing. I really don't know what I'm saying, but I do know I can make this name 255 characters, so I'm just going to put a bunch of "a"s until I get to 255 aaaaaaaaaaaaaaaaaaaaa <55938439+codingMASTER398@users.noreply.github.com>
    Date:   Sun Jul 25 12:02:01 2021 +1000
    
        Higlighter
    
    commit 744394f438a839075c3ef7ade6a1dd90691ea918
    Merge: 7e69be27 69cd5cc5
    Author: A person somewhere that does something for the reason that he likes to do that thing. I really don't know what I'm saying, but I do know I can make this name 255 characters, so I'm just going to put a bunch of "a"s until I get to 255 aaaaaaaaaaaaaaaaaaaaa <55938439+codingMASTER398@users.noreply.github.com>
    Date:   Sun Jul 25 10:03:33 2021 +1000
    
        Merge branch 'master' into master
    
    commit 7e69be2772a0bb4cedbe9ea0fcab5457f8053e44
    Author: A person somewhere that does something for the reason that he likes to do that thing. I really don't know what I'm saying, but I do know I can make this name 255 characters, so I'm just going to put a bunch of "a"s until I get to 255 aaaaaaaaaaaaaaaaaaaaa <55938439+codingMASTER398@users.noreply.github.com>
    Date:   Sun Jul 25 10:01:57 2021 +1000
    
        Add files via upload
    
    commit f53e3e6c576f37c2d476310c5e73186fc04368ea
    Author: A person somewhere that does something for the reason that he likes to do that thing. I really don't know what I'm saying, but I do know I can make this name 255 characters, so I'm just going to put a bunch of "a"s until I get to 255 aaaaaaaaaaaaaaaaaaaaa <55938439+codingMASTER398@users.noreply.github.com>
    Date:   Sat Jul 24 17:12:22 2021 +1000
    
        Add files via upload
    
    commit 07b6da704944711f337f6beaff2ef923b88a63c5
    Author: A person somewhere that does something for the reason that he likes to do that thing. I really don't know what I'm saying, but I do know I can make this name 255 characters, so I'm just going to put a bunch of "a"s until I get to 255 aaaaaaaaaaaaaaaaaaaaa <55938439+codingMASTER398@users.noreply.github.com>
    Date:   Sat Jul 24 17:12:03 2021 +1000
    
        Create addon.json
    
    commit c450e33a6d0752906ea2487320947e0d6984793c
    Author: A person somewhere that does something for the reason that he likes to do that thing. I really don't know what I'm saying, but I do know I can make this name 255 characters, so I'm just going to put a bunch of "a"s until I get to 255 aaaaaaaaaaaaaaaaaaaaa <55938439+codingMASTER398@users.noreply.github.com>
    Date:   Sat Jul 24 17:10:50 2021 +1000
    
        Add files via upload

---
+++ addons/colored-follower-projects/userscript.js
@@ -0,0 +1,41 @@
+export default async function ({ addon, global, console }) {
+  console.log("Running");
+  if (addon.auth.isLoggedIn) {
+    // If they are logged in, activate the rest of the code
+    var username = addon.auth.username; // Get the username
+
+    async function getfollowing(offset) {
+      // Async function to get an array of following using the scratch api.
+      return new Promise((resolve, reject) => {
+        var returned = []; //What the function will return
+        fetch("https://api.scratch.mit.edu/users/" + username + "/following?offset=" + offset + "&limit=40").then(
+          async function (resp) {
+            //Fetch 40 results from the scratch servers
+            resp = await resp.json(); //Make it JSON
+            for (let i = 0; i < resp.length; i++) {
+              //For all following
+              returned.push(resp[i].username); //Add their username to the returned array
+            }
+            if (returned.length == 40) {
+              //Because of scratch limits, we can only get 40 following at a time. If there are more following we have to get, call the function again
+              returned = returned.concat(await getfollowing(offset + 40));
+            }
+            resolve(returned); //Return the array
+          }
+        );
+      });
+    }
+
+    var following = await getfollowing(0); //Getting the following!
+
+    while (true) {
+      const element = await addon.tab.waitForElement("div.thumbnail-creator", {
+        markAsSeen: true,
+      });
+      if (following.includes(element.querySelector("a").innerText)) {
+        //If we are following them...
+        element.parentElement.parentElement.parentElement.style.backgroundColor = addon.settings.get("color");
+      }
+    }
+  }
+}
