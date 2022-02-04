1a059271850e2f95d266bfed1e079ded40d2ce0a
lisa-wolfgang <lisa-wolfgang@users.noreply.github.com>
2022-02-04 03:56:06 +0000

    Format code

--- content-scripts/cs.js
+++ content-scripts/cs.js
@@ -54,7 +54,10 @@ chrome.runtime.sendMessage({ contentScriptReady: { url: location.href } }, onRes
 
 const DOLLARS = ["$1", "$2", "$3", "$4", "$5", "$6", "$7", "$8", "$9"];
 
-const promisify = (callbackFn) => (...args) => new Promise((resolve) => callbackFn(...args, resolve));
+const promisify =
+  (callbackFn) =>
+  (...args) =>
+    new Promise((resolve) => callbackFn(...args, resolve));
 
 let _page_ = null;
 let globalState = null;
@@ -367,16 +370,8 @@ async function onInfoAvailable({ globalState: globalStateMsg, addonsWithUserscri
     } else if (request.fireEvent) {
       _page_.fireEvent(request.fireEvent);
     } else if (request.dynamicAddonEnabled) {
-      const {
-        scripts,
-        userstyles,
-        cssVariables,
-        addonId,
-        injectAsStyleElt,
-        index,
-        dynamicEnable,
-        dynamicDisable,
-      } = request.dynamicAddonEnabled;
+      const { scripts, userstyles, cssVariables, addonId, injectAsStyleElt, index, dynamicEnable, dynamicDisable } =
+        request.dynamicAddonEnabled;
       addStyle({ styles: userstyles, addonId, injectAsStyleElt, index });
       if (everLoadedAddons.find((addon) => addon.addonId === addonId)) {
         if (!dynamicDisable) return;
