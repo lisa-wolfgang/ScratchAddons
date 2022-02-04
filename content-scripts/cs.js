9e9e38244d773512d54a77636681102e25d711e5
lisa-wolfgang <lisawolfgang.onscratch@gmail.com>
2022-02-03 21:54:18 -0600

    `debugger`: fix go-to-block feature
    commit 18044595fbc514b0cd0a1d3811a69128cd8bc0d9
    Author: Thomas Weber <muffin@muffin.ink>
    Date:   Thu Feb 3 21:37:45 2022 -0600
    
        debugger: fix go to block
    
    commit 570a232c4af09c4e630807af07d2f1047014019d
    Author: World_Languages <mundofinky@gmail.com>
    Date:   Wed Feb 2 22:37:37 2022 -0300
    
        Bump to v1.24.1 stable (#4174)
    
    commit 37c052f53d25fab821a44efb92ec1c564e2e9286
    Author: scratchaddons-bot[bot] <73682299+scratchaddons-bot[bot]@users.noreply.github.com>
    Date:   Wed Feb 2 22:14:59 2022 -0300
    
        New strings from Transifex (#4173)
    
        Co-authored-by: scratchaddons-bot[bot] <73682299+scratchaddons-bot[bot]@users.noreply.github.com>
    
    commit 60aab1066cc5ec988dd7ce5d3183257f2a527d0c
    Author: Maximouse <51849865+mxmou@users.noreply.github.com>
    Date:   Thu Feb 3 02:12:04 2022 +0100
    
        Attempt to fix broken profile layout on macOS (#4156)
    
    commit f730cb595356262f212318d7aa80411b8825179a
    Author: Maximouse <51849865+mxmou@users.noreply.github.com>
    Date:   Thu Feb 3 02:07:23 2022 +0100
    
        Fix My Stuff tab problems (#4168)
    
    commit 8d384cc481abae63e1e28c817e251f25f95b64ee
    Author: Maximouse <51849865+mxmou@users.noreply.github.com>
    Date:   Thu Feb 3 02:06:07 2022 +0100
    
        Fix featured project title overflow (#4169)
    
    commit 58f0ba330a6526e6a7506d17eeb83d141e6e9a40
    Author: apple502j <33279053+apple502j@users.noreply.github.com>
    Date:   Thu Feb 3 10:05:47 2022 +0900
    
        scratchr2: fix z-index (#4166)
    
    commit 2f6487b807f7850c1daf0b1d78490c33e716e439
    Author: apple502j <33279053+apple502j@users.noreply.github.com>
    Date:   Wed Feb 2 23:13:16 2022 +0900
    
        Bump to 1.24.1-pre (#4165)
    
    commit e5eeda4cde3b99213328453af64fd82fc96ee9fc
    Author: scratchaddons-bot[bot] <73682299+scratchaddons-bot[bot]@users.noreply.github.com>
    Date:   Wed Feb 2 11:49:19 2022 +0900
    
        New strings from Transifex (#4162)
    
        Co-authored-by: scratchaddons-bot[bot] <73682299+scratchaddons-bot[bot]@users.noreply.github.com>
    
    commit f8f112fdb31a7158435ef067177935882aee16a4
    Author: scratchaddons-bot[bot] <73682299+scratchaddons-bot[bot]@users.noreply.github.com>
    Date:   Mon Jan 31 19:50:05 2022 -0500
    
        New strings from Transifex (#4152)
    
        Co-authored-by: scratchaddons-bot[bot] <73682299+scratchaddons-bot[bot]@users.noreply.github.com>
    
    commit a6d4e8fdbba33d8060789ba09251ece8e05308b0
    Author: World_Languages <mundofinky@gmail.com>
    Date:   Mon Jan 31 20:17:41 2022 -0300
    
        Bump to v1.24.0 stable (#4151)
    
    commit 05f044c3692bf4a4a12a3f420890d2618466bddf
    Author: World_Languages <mundofinky@gmail.com>
    Date:   Mon Jan 31 20:13:38 2022 -0300
    
        Add YouTube video to update notification (#4150)

--- content-scripts/cs.js
+++ content-scripts/cs.js
@@ -54,10 +54,7 @@ chrome.runtime.sendMessage({ contentScriptReady: { url: location.href } }, onRes
 
 const DOLLARS = ["$1", "$2", "$3", "$4", "$5", "$6", "$7", "$8", "$9"];
 
-const promisify =
-  (callbackFn) =>
-  (...args) =>
-    new Promise((resolve) => callbackFn(...args, resolve));
+const promisify = (callbackFn) => (...args) => new Promise((resolve) => callbackFn(...args, resolve));
 
 let _page_ = null;
 let globalState = null;
@@ -370,8 +367,16 @@ async function onInfoAvailable({ globalState: globalStateMsg, addonsWithUserscri
     } else if (request.fireEvent) {
       _page_.fireEvent(request.fireEvent);
     } else if (request.dynamicAddonEnabled) {
-      const { scripts, userstyles, cssVariables, addonId, injectAsStyleElt, index, dynamicEnable, dynamicDisable } =
-        request.dynamicAddonEnabled;
+      const {
+        scripts,
+        userstyles,
+        cssVariables,
+        addonId,
+        injectAsStyleElt,
+        index,
+        dynamicEnable,
+        dynamicDisable,
+      } = request.dynamicAddonEnabled;
       addStyle({ styles: userstyles, addonId, injectAsStyleElt, index });
       if (everLoadedAddons.find((addon) => addon.addonId === addonId)) {
         if (!dynamicDisable) return;
