f10126c200d72e55df332f85dee7f11008512940
lisa-wolfgang <lisa-wolfgang@users.noreply.github.com>
2022-02-05 03:47:41 +0000

    Format code

--- addons/debugger/performance.js
+++ addons/debugger/performance.js
@@ -4,8 +4,6 @@ export default async function createPerformanceTab({ debug, addon, console, msg 
   const vm = addon.tab.traps.vm;
 
   await addon.tab.loadScript(addon.self.lib + "/thirdparty/cs/chart.min.js");
-  
-      
 
   const tab = debug.createHeaderTab({
     text: msg("tab-performance"),
@@ -81,12 +79,14 @@ export default async function createPerformanceTab({ debug, addon, console, msg 
     type: "line",
     data: {
       labels,
-      datasets: [{
+      datasets: [
+        {
           data: Array(NUMBER_OF_POINTS).fill(-1),
           borderWidth: 1,
-          fill: !addon.settings.get("graphs_optimization"), 
+          fill: !addon.settings.get("graphs_optimization"),
           backgroundColor: "hsla(163, 85%, 40%, 0.5)",
-      }],
+        },
+      ],
     },
     options: {
       animation: !addon.settings.get("graphs_optimization"),
